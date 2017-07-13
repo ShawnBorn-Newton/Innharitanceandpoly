32ecbca5152872758743d51597939c22bfb0bc6d
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-07-13 10:50:15 -0400

    boats

--- inharitanceandpoly/Program.cs
+++ inharitanceandpoly/Program.cs
@@ -10,6 +10,10 @@ namespace inharitanceandpoly
     {
         static void Main(string[] args)
         {
+            Boat boat1 = new inharitanceandpoly.Boat(2, 500, "white and Yellow", 70.0d, .78d);
+            boat1.Move();
+            boat1.Move();
+            Console.WriteLine("Boat " + boat1.GetDistanceTraveled());
         }
     }
 }
